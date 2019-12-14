9058dd6ea04d51aa580d2599f74e06aebde548ae
LeeVaughn <dlvprogramming@gmail.com>
2019-12-14 10:15:51 -0800

    created initial scraper file and started building out Scraper class

---
+++ scraper.py
@@ -0,0 +1,10 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+class Scraper:
+  # takes the website to scrape as a parameter
+  def __init__(self, site):
+    self.site = site
+
+  def scrape(self):
+    pass
