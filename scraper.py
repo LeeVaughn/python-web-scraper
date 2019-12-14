465e5e5a7d1bbd309dd8de548828813118b177b5
LeeVaughn <dlvprogramming@gmail.com>
2019-12-14 10:45:26 -0800

    finished building out scraper method

--- scraper.py
+++ scraper.py
@@ -7,4 +7,22 @@ class Scraper:
     self.site = site
 
   def scrape(self):
-    pass
+    # makes a request to the site and returns response object
+    r = urllib.request.urlopen(self.site)
+    # returns HTML
+    html = r.read()
+    parser= "html.parser"
+    # parses html
+    sp = BeautifulSoup(html, parser)
+
+    # find all a tags then loops over them to get href value of each
+    for tag in sp.find_all("a"):
+      url = tag.get("href")
+      print(url)
+      if url is None:
+        continue
+      if "https" in url:
+        print(url)
+
+urls = "https://teamtreehouse.com/"
+Scraper(urls).scrape()
